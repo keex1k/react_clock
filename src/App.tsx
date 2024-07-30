8a1ff75953afadf696ad9060a0a01ae6509a3dae
Bartek <kowalski_1410@wp.pl>
2024-07-31 00:36:15 +0200

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,70 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock'
 
-function getRandomName(): string {
-  const value = Date.now().toString().slice(-4);
-
-  return `Clock-${value}`;
+type State = {
+  hasClock: boolean;
+  clockName: string;
 }
 
+export class App extends React.Component<{}, State> {
+  state = {
+    hasClock: true,
+    clockName: '0'
+  }
+
+  handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false })
+  }
+
+  handleLeftClick = (event: MouseEvent) => {
+    if (event.button === 0) { // Sprawdzamy czy kliknięto lewy przycisk myszy
+      this.setState({ hasClock: true });
+    }
+  }
+
+  componentDidMount(): void {
+    document.addEventListener('contextmenu', this.handleRightClick);
+    document.addEventListener('click', this.handleLeftClick);
+    window.setInterval(() => {
+      this.setState({ clockName: Date.now().toString().slice(-4)})
+    }, 3300);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('contextmenu', this.handleRightClick);
+    document.removeEventListener('click', this.handleLeftClick);
+  }
+
+  componentDidUpdate(_prevProps: Readonly<{}>, prevState: Readonly<State>, _snapshot?: any): void {
+    console.debug(`Renamed from ${prevState.clockName} to ${this.state.clockName}`)
+  }
+
+  render() {
+    const content = (
+      <div className="Clock">
+        <strong className="Clock__name">Clock-{this.state.clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          <Clock name = {this.state.clockName} />
+        </span>
+      </div>
+      )
+    return (
+      <div className="App">
+      <h1>React clock</h1>
+
+      {this.state.hasClock ? content : null}
+    </div>
+    );
+  }
+}
+/*
 export const App: React.FC = () => {
-  const today = new Date();
+  //const today = new Date();
   let clockName = 'Clock-0';
 
   // This code starts a timer
@@ -29,9 +85,10 @@ export const App: React.FC = () => {
         {' time is '}
 
         <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
+          <Clock />
         </span>
       </div>
     </div>
   );
 };
+*/
---
+++ src/Clock.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+type State = {
+  time: Date
+}
+
+type Props = {
+  name: string
+}
+
+export class Clock extends React.Component <Props, State> {
+  state = {
+    time: new Date()
+  }
+
+  timerId = 0
+
+  componentDidMount(): void {
+    this.timerId = window.setInterval(() => {
+      this.setState({
+        time: new Date(this.state.time.getTime() + 1000)
+      })
+      // eslint-disable-next-line no-console
+      console.log(this.state.time.toUTCString().slice(-12, -4));
+    },1000)
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    return(
+      this.state.time.toUTCString().slice(-12, -4)
+    );
+  }
+}
