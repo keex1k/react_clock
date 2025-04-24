9dbdbdcd51331be4a25b710fd52bedc559d095a3
Bartek <kowalski_1410@wp.pl>
2025-04-25 00:11:55 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,31 +8,73 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+type State = {
+  hasClock: boolean,
+  clockName: string,
+}
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+export class App extends React.Component<{}, State>{
+  state = {
+    hasClock: true,
+    clockName: 'Clock-0'
+  }
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  intervalId = 0;
 
-  return (
-    <div className="App">
-      <h1>React clock</h1>
+  handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    this.setState({ hasClock: false });
+    window.clearInterval(this.intervalId);
+  };
 
+  handleLeftClick = (event: MouseEvent) => {
+    if (event.button === 0) {
+      // Sprawdzamy czy kliknięto lewy przycisk myszy
+      this.setState({ hasClock: true });
+      this.intervalId = window.setInterval(() => {
+        this.setState({ clockName: getRandomName() });
+      }, 3300);
+    }
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('contextmenu', this.handleRightClick);
+    document.addEventListener('click', this.handleLeftClick);
+    this.intervalId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName()});
+    }, 3300);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('contextmenu', this.handleRightClick);
+    document.removeEventListener('click', this.handleLeftClick);
+  }
+
+  componentDidUpdate(
+    _prevProps: Readonly<{}>,
+    prevState: Readonly<State>,
+  ): void {
+    if (this.state.clockName !== prevState.clockName) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
+      );
+    }
+  }
+
+  render(){
+    const content = (
       <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
+        <Clock name = {this.state.clockName} />
       </div>
-    </div>
-  );
+    );
+
+    return (
+      <div className = "App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock ? content : null}
+      </div>
+    );
+  }
 };
